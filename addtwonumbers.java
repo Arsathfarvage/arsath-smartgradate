ee38aa1e2b7a4a950bfb00f3f61662c3024640e1
Arsathfarvage <113118956+Arsathfarvage@users.noreply.github.com>
2022-10-04 21:19:27 +0530

    Add files via upload

--- addtwonumbers.java
+++ addtwonumbers.java
@@ -6,7 +6,6 @@ public class addtwonumbers {
 		 int first = 100;
 		int second = 200;
 		 System.out.println(first + " " + second);
-		 // add two numbers
 		int sum = first + second;
 		 System.out.println("The sum is: " + sum);
 		 } 
